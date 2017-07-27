215d3f192ecc9910cdddc326cb7f238c3eada6be
Jason Bockmon <jbockmon@gmail.com>
2017-07-27 15:24:33 -0500

    Level builder is completed. All tiles utilized in level 1 supported, and Level 1 rendering with a subarray of collision enabled tiles completed.

--- testScript.js
+++ testScript.js
@@ -11,7 +11,9 @@ let Container = PIXI.Container,
 
 //Create stage and renderer 
 let stage = new Container();
+//let renderer = autoDetectRenderer(720, 528);
 let renderer = autoDetectRenderer(1440, 176);
+
 document.body.appendChild(renderer.view);
 
 renderer.view.style.border = "1px solid black";
@@ -23,7 +25,9 @@ loader
         "images/PNG/spritesheets/player/player-idle.png",
         "images/PNG/environment/layers/background.png",
         "images/PNG/environment/layers/middleground.png",
-        "images/PNG/environment/layers/tilesets.png"
+        "images/PNG/environment/layers/tilesets.png",
+        "images/PNG/environment/layers/props.png",
+        "images/PNG/levels/Level1v2.png"
     ])
     .on("progress", loadProgressHandler)
     .load(setup);
@@ -40,6 +44,10 @@ let left = keyboard(37),
     down = keyboard(40);
 
 function setup() {
+    //stage.position sets where the game attaches to the canvas.
+    //stage.position.set(0, 0);
+    //stage.pivot.set moves the camera. 
+    //stage.pivot.set(0, 0);
     
     //Setting up the background
     bgBack = new TilingSprite(resources["images/PNG/environment/layers/background.png"].texture, 1440, 176);
@@ -47,11 +55,16 @@ function setup() {
     bgBack.position.y = 0;
     bgBack.tilePosition.x = 0;
     bgBack.tilePosition.y = 0;
+    //bgBack.scale.set(4,4);
     bgFront = new TilingSprite(resources["images/PNG/environment/layers/middleground.png"].texture, 1440, 176);
     bgFront.position.x = 0;
     bgFront.position.y = 0;
     bgFront.tilePosition.x = 0;
     bgFront.tilePosition.y = 0;
+    //bgFront.scale.set(4,4);
+    
+    
+    
     stage.addChild(bgBack);
     stage.addChild(bgFront);
     
@@ -67,11 +80,14 @@ function setup() {
     
     
     playerSprite.x = 10;
-    playerSprite.y = 10;
+    playerSprite.y = 80;
     
     playerSprite.interactive = false;
     playerSprite.vx = 0;
     playerSprite.vy = 0;
+    
+    //playerSprite.scale.set(4,4);
+    
     stage.addChild(playerSprite);   
     
     
@@ -179,11 +195,6 @@ function play () {
                 playerSprite.vx = 0; 
         }
     }
-    
-    
-    
-    
-    
 }
 
 function frame(source, x, y, width, height) {
@@ -219,19 +230,187 @@ function loadProgressHandler(loader, resource) {
 
 function buildLevel(level) {
     let rows = 0,
-        cols = 0;
+        cols = 0,
+        i = 0,
+        tx = 0,
+        ty = 0;
     let collArr = [];
+    let sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 0, 0, 16, 16));;
     
-    for( rows = 0; rows < level.height; rows++){
-        for( cols = 0; cols < level.width; cols++){
-            let sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 336, 96, 16, 16));
-            if( level.layers[1].data[(level.width*rows+cols)] == 166){
-                sprite.x = cols * level.tilewidth;
-                sprite.y = rows * level.tileheight;
-                stage.addChild(sprite);
-                collArr.push(sprite);
-            }    
+    for( var layer = 0; layer < 3; layer++){
+        for( rows = 0; rows < level.height; rows++){
+            for( cols = 0; cols < level.width; cols++){
+                
+                // setting up iterator and tilex and tiley for clarity
+                i = (level.width*rows+cols);     
+                tx = (((level.layers[layer].data[i] % 24) * level.tilewidth) - level.tilewidth);
+                ty = (Math.floor(level.layers[layer].data[i]/24) * level.tilewidth)
+                console.log(i);
+                switch(level.layers[layer].data[i]){
+                    //no tile
+                    case 0: 
+                        break;
+                    //invisible tile, can collide with player sprite
+                    case 1:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 0, 0, 16, 16));
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        collArr.push(sprite);
+                        break;  
+                    case 316:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/props.png", 192, 16, 16, 16));
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 331:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/props.png", 192, 32, 16, 16));
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 346:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/props.png", 192, 48, 16, 16));
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        collArr.push(sprite);
+                        break;
+                    case 3221225559:
+                        break;
+                    case 2684354780:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 48, 144, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI/2;
+                        sprite.anchor.x = 0;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 2684354804:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 48, 160, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI/2;
+                        sprite.anchor.x = 0;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 2684354816:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 240, 160, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI/2;
+                        sprite.anchor.x = 0;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 2684354840:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 240, 176, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI/2;
+                        sprite.anchor.x = 0;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 3221225618:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 16, 96, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 3221225620:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 48, 96, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 3221225621:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 64, 96, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break; 
+                    case 3221225642:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 16, 112, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 3221225644:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 64, 112, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 3221225645:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 48, 112, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    case 3221225666:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", 16, 128, 16, 16));
+                        sprite.anchor.x = 0.5;
+                        sprite.anchor.y = 0.5;
+                        sprite.rotation = Math.PI;
+                        sprite.anchor.x = 1;
+                        sprite.anchor.y = 1;
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                        break;
+                    //The rest of the tiles do not get added to collision array to save cpu cycles
+                    default:
+                        sprite = new Sprite(frame("images/PNG/environment/layers/tilesets.png", tx, ty, level.tilewidth,            level.tilewidth));
+                        sprite.x = cols * level.tilewidth;
+                        sprite.y = rows * level.tileheight;
+                        stage.addChild(sprite);
+                   
+                }
+            }
         }
+        
     }
     
     return collArr;
