80bfd760b0d337620a8cc64656e4d5ffcaddfc9e
Jason Bockmon <jbockmon@gmail.com>
2017-08-04 15:15:15 -0500

    Added animations and broke up functions into new script files

---
+++ frame.js
@@ -0,0 +1,24 @@
+function frame(source, x, y, width, height) {
+    let texture, imageFrame;
+    
+    //If the source is a string, it's either a texture in the
+    //cache or an image file
+    if (typeof source === "string") {
+        if (resources[source].texture){
+            texture = new Texture(resources[source].texture);
+        }
+    }
+    //If the `source` is a texture, use it
+    else if (source instanceof Texture) {
+        texture = new Texture(source);
+    }
+    
+    if(!texture) {
+        console.log(`Please load the ${source} texture into the cache.`);
+    } else {
+        //Make a rectangle the size of the sub-image
+        imageFrame = new Rectangle(x, y, width, height);
+        texture.frame = imageFrame;
+        return texture;
+    }
+}
